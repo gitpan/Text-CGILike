cbcc125432b66d17ff376671cb9cc6f5048879f5
celogeek <me@celogeek.com>
2012-07-18 14:39:36 +0000

    Import of CELOGEEK/Text-CGILike-0.4 from CPAN.
    gitpan-cpan-distribution: Text-CGILike
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         CELOGEEK/Text-CGILike-0.4.tar.gz
    gitpan-cpan-author:       CELOGEEK
    gitpan-cpan-maturity:     released

--- lib/Text/CGILike.pm
+++ lib/Text/CGILike.pm
@@ -14,25 +14,9 @@ use strict;
 use warnings;
 use Moo;
 use Text::Format;
+use Carp;
 
-our $VERSION = '0.3';    # VERSION
-
-our ( @ISA, @EXPORT, %EXPORT_TAGS );
-
-BEGIN {
-    require Exporter;
-
-    my @html2    = qw/start_html end_html hr h1 ul li br meta/;
-    my @netscape = qw/center/;
-
-    @ISA         = qw(Exporter);
-    @EXPORT      = ( @html2, @netscape );
-    %EXPORT_TAGS = (
-        'html2'    => [@html2],
-        'netscape' => [@netscape],
-        'standard' => [@EXPORT]
-    );
-}
+our $VERSION = '0.4';    # VERSION
 
 my $_DEFAULT_CLASS;
 
@@ -119,12 +103,11 @@ sub _self_or_default {
 }
 
 sub _center {
-    my $self = shift;
-    my ( $left, $right, $text ) = @_;
-    $left  = "" unless defined $left;
-    $right = "" unless defined $right;
+    my ( $self, $left_pad, $right_pad, $text ) = @_;
+    $left_pad  = "" unless defined $left_pad;
+    $right_pad = "" unless defined $right_pad;
 
-    my $size = $self->columns - length($left) - length($right);
+    my $size = $self->columns - length($left_pad) - length($right_pad);
     my $TF   = Text::Format->new(
         { firstIndent => 0, bodyIndent => 0, columns => $size } );
 
@@ -132,26 +115,107 @@ sub _center {
     return join(
         "",
         map {
-            $_ = $TF->center($_);
-            chomp;
-            sprintf( $left . "%-" . $size . "s" . $right . "\n", $_ );
-            } @texts
+            sprintf( $left_pad . "%-" . $size . "s" . $right_pad . "\n", $_ )
+            }
+            map {
+            do { chomp; $_ }
+            }
+            map { $TF->center($_) } @texts
     );
 
 }
 
 sub _left {
-    my $self = shift;
-    my ( $left, $text ) = @_;
-    $left = "" unless defined $left;
+    my ( $self, $left_pad, $text ) = @_;
+    $left_pad = "" unless defined $left_pad;
 
-    my $size = $self->columns - length($left);
+    my $size = $self->columns - length($left_pad);
     my $TF   = Text::Format->new(
         { firstIndent => 0, bodyIndent => 0, columns => $size } );
 
     my @texts = $TF->format($text);
-    return $left . join( "" . $left, @texts );
+    return $left_pad . join( "" . $left_pad, @texts );
 
+}
+
+sub _expand_arr {
+    my ( $tags, @arr ) = @_;
+    my @res;
+    for my $tag (@arr) {
+        if ( substr( $tag, 0, 1 ) eq ':' ) {
+            push @res, _expand_arr( $tags, @{ $tags->{$tag} } );
+        }
+        else {
+            push @res, $tag;
+        }
+    }
+    return @res;
+}
+
+my %EXPORT_MAP = (
+    ':html2' => [
+        'h1' .. 'h6', qw/p br hr ol ul li dl dt dd menu code var strong em
+            tt u i b blockquote pre img a address cite samp dfn html head
+            base body Link nextid title meta kbd start_html end_html
+            input Select option comment charset escapeHTML/
+    ],
+    ':html3' => [
+        qw/div table caption th td TR Tr sup Sub strike applet Param nobr
+            embed basefont style span layer ilayer font frameset frame script small big Area Map/
+    ],
+    ':html4' => [
+        qw/abbr acronym bdo col colgroup del fieldset iframe
+            ins label legend noframes noscript object optgroup Q
+            thead tbody tfoot/
+    ],
+    ':netscape' => [qw/blink fontsize center/],
+    ':form'     => [
+        qw/textfield textarea filefield password_field hidden checkbox checkbox_group
+            submit reset defaults radio_group popup_menu button autoEscape
+            scrolling_list image_button start_form end_form startform endform
+            start_multipart_form end_multipart_form isindex tmpFileName uploadInfo URL_ENCODED MULTIPART/
+    ],
+    ':cgi' => [
+        qw/param upload path_info path_translated request_uri url self_url script_name
+            cookie Dump
+            raw_cookie request_method query_string Accept user_agent remote_host content_type
+            remote_addr referer server_name server_software server_port server_protocol virtual_port
+            virtual_host remote_ident auth_type http append
+            save_parameters restore_parameters param_fetch
+            remote_user user_name header redirect import_names put
+            Delete Delete_all url_param cgi_error/
+    ],
+    ':ssl'     => [qw/https/],
+    ':cgi-lib' => [qw/ReadParse PrintHeader HtmlTop HtmlBot SplitParam Vars/],
+    ':html'    => [qw/:html2 :html3 :html4 :netscape/],
+    ':standard' => [qw/:html2 :html3 :html4 :form :cgi/],
+    ':push' =>
+        [qw/multipart_init multipart_start multipart_end multipart_final/],
+    ':all' => [qw/:html2 :html3 :netscape :form :cgi :internal :html4/]
+);
+
+sub import {
+    my ( $self, $to_import_str ) = @_;
+    my @to_import = $to_import_str =~ /(:?\w+)/gx;
+    my $caller = caller;
+
+    for my $sym ( _expand_arr( \%EXPORT_MAP, @to_import ) ) {
+        unless ( $caller->can($sym) ) {
+            my $meth = $self->can($sym) // sub {
+                carp "Missing tag : ", $sym;
+                return join( '', @_ );
+            };
+
+            {
+                ## no critic qw(ProhibitNoStrict)
+                no strict qw/refs/;
+                *{"${caller}::$sym"} = $meth;
+                ## use critic
+            }
+        }
+    }
+
+    return;
 }
 
 1;
@@ -164,7 +228,15 @@ Text::CGILike - Wrapper to create text file using the CGI syntax
 
 =head1 VERSION
 
-version 0.3
+version 0.4
+
+=head1 OVERVIEW
+
+CGI is an old module, and now we can create html or text with a simple template.
+
+I have create this module to be able to format my email in html or text by just changing the module I use.
+
+So I don't use template in that case, just a simple '--format=text/html'
 
 =head1 ATTRIBUTES
 
@@ -182,15 +254,71 @@ To change columns using keywords
     my ($TCGI) = Text::CGILike->DEFAULT_CLASS;
     $TCGI->columns($columns);
 
+=head1 METHODS
+
+=head2 DEFAULT_CLASS
+
+This singleton is use if you don't instanciate Text::CGILike
+
+=head2 start_html
+
+Start the document, you can pass headers like CGI here. Only '-title' will be used.
+
+    start_html('my title');
+    start_html(-title => 'my title');
+
+=head2 end_html
+
+Finish the document.
+
+    end_html;
+
+=head2 meta
+
+Completly ignore. no meta in brute text
+
+=head2 h1
+
+Create a box that define the bigger text.
+
+    h1('my big text');
+
+=head2 hr
+
+Create a row of '#' (horizontal rule)
+
+=head2 br
+
+break line
+
+=head2 center
+
+center the text, and respect wrap of text
+
+=head2 ul
+
+create list
+
+=head2 li
+
+do list starting with an asterix '*'
+
+=head2 import
+
+Import tags. check L<CGI> for more information.
+
 =head1 SEE ALSO
 
 L<CGI>
 
 =head1 BUGS
 
-Any bugs or evolution can be submit here :
+Please report any bugs or feature requests on the bugtracker website
+https://github.com/celogeek/Text-CGILike/issues
 
-L<Github|https://github.com/celogeek/Text-CGILike>
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
@@ -206,3 +334,4 @@ the same terms as the Perl 5 programming language system itself.
 =cut
 
 __END__
+
